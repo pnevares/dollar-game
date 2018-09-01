1ea0cb39657256acda34c2d160678ac792a507ed
Pablo Nevares <pablo@nevares.com>
2018-09-01 15:25:43 -0700

    Example game graph

--- src/index.js
+++ src/index.js
@@ -1,23 +1,62 @@
 import Sigma from 'sigma';
 
 var s = new Sigma('root');
+
 s.graph.addNode({
   id: 'n0',
-  label: 'Hello',
-  x: 0,
+  label: '1',
+  x: 1,
   y: 0,
-  size: 1,
-  color: '#f00'
+  size: 2,
+  color: '#000'
 }).addNode({
   id: 'n1',
-  label: 'World !',
-  x: 1,
+  label: '-1',
+  x: 0,
   y: 1,
-  size: 1,
-  color: '#00f'
+  size: 2,
+  color: '#000'
+}).addNode({
+  id: 'n2',
+  label: '-2',
+  x: 2,
+  y: 1,
+  size: 2,
+  color: '#000'
+}).addNode({
+  id: 'n3',
+  label: '2',
+  x: 0,
+  y: 2,
+  size: 2,
+  color: '#000'
+}).addNode({
+  id: 'n4',
+  label: '3',
+  x: 1,
+  y: 2,
+  size: 2,
+  color: '#000'
 }).addEdge({
   id: 'e0',
   source: 'n0',
-  target: 'n1'
+  target: 'n4'
+}).addEdge({
+  id: 'e1',
+  source: 'n0',
+  target: 'n2'
+}).addEdge({
+  id: 'e2',
+  source: 'n1',
+  target: 'n2'
+}).addEdge({
+  id: 'e3',
+  source: 'n2',
+  target: 'n4'
+}).addEdge({
+  id: 'e4',
+  source: 'n3',
+  target: 'n4'
 });
+
 s.refresh();
