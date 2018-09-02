dace6c21aa691eb2fe2ee835d0ab16b5c9ee1520
Pablo Nevares <pablo@nevares.com>
2018-09-02 02:51:18 -0700

    Checking for success inside set-colors

--- src/set-colors.js
+++ src/set-colors.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
 
 export default function setColors(s) {
+  let reds = 0;
+
   s.graph.nodes().forEach((node) => {
     const value = Number.parseInt(node.label, 10);
 
@@ -8,6 +10,11 @@ export default function setColors(s) {
       s.graph.nodes(node.id).color = '#0f0'; // green
     } else {
       s.graph.nodes(node.id).color = '#f00'; // red
+      reds += 1;
     }
   });
+
+  if (reds === 0) {
+    console.log('YOU WIN'); // eslint-disable-line no-console
+  }
 }
