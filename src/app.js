edf4dbf8ceed359fe22c2831dfe9ee568ce1b9ab
Pablo Nevares <pablo@nevares.com>
2018-09-02 19:29:11 -0700

    Puzzle component

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import createGraph from './create-graph';
 import puzzles from './puzzles';
+import Puzzle from './puzzle';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      puzzleIndex: 0,
+    };
+
+    this.changePuzzle = this.changePuzzle.bind(this);
+  }
+
   componentDidMount() {
     createGraph(puzzles[0]);
   }
 
+  changePuzzle(index) {
+    this.setState({
+      puzzleIndex: index,
+    });
+  }
+
   render() {
+    const { puzzleIndex } = this.state;
     return (
-      <div id="container" />
+      <Puzzle puzzleIndex={puzzleIndex} />
     );
   }
 }
