5d8384d17bcba0dda05dc9e72c0a170a31c37a4e
Pablo Nevares <pablo@nevares.com>
2018-09-04 20:38:29 -0700

    Added placeholder button to build-your-own

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -3,18 +3,29 @@ import PropTypes from 'prop-types';
 import puzzles from '../puzzles';
 
 const Buttons = ({ clickHandler }) => (
-  <p>
-    Puzzle select:
-    {Object.keys(puzzles).map(v => (
+  <>
+    <p>
       <button
         type="button"
-        key={v}
-        onClick={() => clickHandler(v)}
+        key="build-your-own"
+        onClick={() => console.log('build-your-own')}
       >
-        {v}
+        build-your-own
       </button>
-    ))}
-  </p>
+    </p>
+    <p>
+      Puzzle select:
+      {Object.keys(puzzles).map(v => (
+        <button
+          type="button"
+          key={v}
+          onClick={() => clickHandler(v)}
+        >
+          {v}
+        </button>
+      ))}
+    </p>
+  </>
 );
 
 Buttons.propTypes = {
